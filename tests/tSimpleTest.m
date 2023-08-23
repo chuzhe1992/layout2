d5e67718f6a9572a107e62b33e2ec041d1a2c281
chuzhe1992 <142944070+chuzhe1992@users.noreply.github.com>
2023-08-23 18:14:30 -0400

    Update tSimpleTest.m

--- tests/tSimpleTest.m
+++ tests/tSimpleTest.m
@@ -4,7 +4,7 @@ classdef tSimpleTest < glttestutilities.TestInfrastructure
 
          function tAddingInvisibleControlIsWarningFree(testCase )
 
-            %ConstructorName = 'uiextras.BoxPanel'; % doesnt matter choosing 'uiextras.BoxPanel' or 'uix.BoxPanel'
+            ConstructorName = 'uiextras.BoxPanel'; % doesnt matter choosing 'uiextras.BoxPanel' or 'uix.BoxPanel'
 
             % Create the component.
             component = testCase.constructComponent(ConstructorName); 
