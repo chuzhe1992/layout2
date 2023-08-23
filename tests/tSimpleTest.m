a891848b3ea0f7d3d86afd60f8479252f5c903ab
chuzhe1992 <142944070+chuzhe1992@users.noreply.github.com>
2023-08-23 17:23:10 -0400

    Update tSimpleTest.m

--- tests/tSimpleTest.m
+++ tests/tSimpleTest.m
@@ -1,4 +1,4 @@
-classdef tSimpleTest < sharedtests.SharedPanelTests
+classdef tSimpleTest < glttestutilities.TestInfrastructure%sharedtests.SharedPanelTests
     %TBOXPANEL Tests for uiextras.BoxPanel and uix.BoxPanel.
 
     properties ( TestParameter )
@@ -103,7 +103,23 @@ classdef tSimpleTest < sharedtests.SharedPanelTests
 
 
         end % tPassingShadowColorToConstructorIsCorrect
- 
+        
+         function tAddingInvisibleControlIsWarningFree( ...
+                testCase, ConstructorName )
+            disp("check 8");
+            % Create the component.
+            component = testCase.constructComponent( ConstructorName );
+
+            % Create an invisible control then reparent it to the
+            % component.
+            button = uicontrol( 'Parent', [], 'Visible', 'off' );
+            testCase.addTeardown( @() delete( button ) )
+            reparenter = @() set( button, 'Parent', component );
+            testCase.verifyWarningFree( reparenter, ...
+                ['Reparenting an invisible control to the ', ...
+                ConstructorName, ' component was not warning-free.'] )            
+            disp("pass 8");
+        end % tAddingInvisibleControlIsWarningFree
 
   end
 
