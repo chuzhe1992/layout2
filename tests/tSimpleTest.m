97c1074ed1d75cb31feca9ecd2575d708c3c637e
chuzhe1992 <142944070+chuzhe1992@users.noreply.github.com>
2023-08-23 18:07:47 -0400

    Update tSimpleTest.m

--- tests/tSimpleTest.m
+++ tests/tSimpleTest.m
@@ -7,12 +7,12 @@ classdef tSimpleTest < glttestutilities.TestInfrastructure
             %ConstructorName = 'uiextras.BoxPanel'; % doesnt matter choosing 'uiextras.BoxPanel' or 'uix.BoxPanel'
 
             % Create the component.
-            %component = testCase.constructComponent(ConstructorName); 
+            component = testCase.constructComponent(ConstructorName); 
 
             % Create an invisible control then reparent it to the
             % component.
-            button = uicontrol( 'Parent', [], 'Visible', 'off' );
-            testCase.addTeardown( @() delete( button ) )
+            %button = uicontrol( 'Parent', [], 'Visible', 'off' );
+            %testCase.addTeardown( @() delete( button ) )
             %reparenter = @() set( button, 'Parent', component );
             %testCase.verifyWarningFree( reparenter, ...
             %    ['Reparenting an invisible control to the ', ...
