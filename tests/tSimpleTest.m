8a0a32c7b1d8cedc7a91a47542c7cebce3a84677
chuzhe1992 <142944070+chuzhe1992@users.noreply.github.com>
2023-08-23 17:56:49 -0400

    Update tSimpleTest.m

--- tests/tSimpleTest.m
+++ tests/tSimpleTest.m
@@ -1,28 +1,11 @@
 classdef tSimpleTest < glttestutilities.TestInfrastructure
-    %TBOXPANEL Tests for uiextras.BoxPanel and uix.BoxPanel.
-
-    properties ( TestParameter )
-        % The constructor name, or class, of the component under test.
-        ConstructorName = {'uix.BoxPanel'}%{'uiextras.BoxPanel', 'uix.BoxPanel'}
-    end 
 
     methods ( Test, Sealed )
 
-        %{
-        function dummyTestPoint( ...
-                testCase, ConstructorName )
+         function tAddingInvisibleControlIsWarningFree(testCase )
 
-            disp("---check here----");
-
-            disp("---pass check---");
-
-        end 
-        %}
-        
-         function tAddingInvisibleControlIsWarningFree(testCase, ConstructorName )
-            disp("check 2");
             % Create the component.
-            component = testCase.constructComponent( ConstructorName );
+            component = testCase.constructComponent( 'uiextras.BoxPanel' ); % doesnt matter if 'uiextras.BoxPanel' or 'uix.BoxPanel'
 
             % Create an invisible control then reparent it to the
             % component.
@@ -32,7 +15,6 @@ classdef tSimpleTest < glttestutilities.TestInfrastructure
             testCase.verifyWarningFree( reparenter, ...
                 ['Reparenting an invisible control to the ', ...
                 ConstructorName, ' component was not warning-free.'] )            
-            disp("pass 2");
         end 
 
   end
